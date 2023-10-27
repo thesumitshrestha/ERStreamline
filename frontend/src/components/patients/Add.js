79a170c43d59285ae2b7135b2cfb74fb9d4fa930
Simran Thapa <simranthapa634@gmail.com>
2023-10-26 23:38:16 -0500

    Style add patient, add health staff and add lab forms

--- frontend/src/components/patients/Add.js
+++ frontend/src/components/patients/Add.js
@@ -35,81 +35,114 @@ const Add = () => {
   };
 
   return (
-    <form className='create' onSubmit={handleSubmit}>
-      <h3> Add a New Patient</h3>
-      <label htmlFor=''>First Name: </label>
-      <input
-        type='text'
-        onChange={(e) => setFirstName(e.target.value)}
-        value={firstName}
-      />{' '}
-      <br />
-      <label htmlFor=''>Last Name: </label>
-      <input
-        type='text'
-        onChange={(e) => setLastName(e.target.value)}
-        value={lastName}
-      />{' '}
-      <br />
-      <label htmlFor=''>Date of Birth: </label>
-      <input
-        type='date'
-        onChange={(e) => setDateofBirth(e.target.value)}
-        value={dateOfBirth}
-      />{' '}
-      <br />
-      <label htmlFor=''>Address: </label>
-      <input
-        type='text'
-        onChange={(e) => setAddress(e.target.value)}
-        value={address}
-      />
-      <br />
-      <label htmlFor=''>Phone: </label>
-      <input
-        type='text'
-        onChange={(e) => setPhone(e.target.value)}
-        value={phone}
-      />
-      <br />
-      <label htmlFor=''>Email: </label>
-      <input
-        type='email'
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <br />
-      <label htmlFor=''>Gender: </label>
-      <select
-        name=''
-        id='gender'
-        value={gender}
-        onChange={(e) => setGender(e.target.value)}
-      >
-        <option value='Male'>Male</option>
-        <option value='Female'>Female</option>
-        <option value='Other'>Other</option>
-      </select>
-      <br />
-      <label htmlFor=''>Blood Group: </label>
-      <select
-        name=''
-        id='gender'
-        value={gender}
-        onChange={(e) => setBloodGroup(e.target.value)}
-      >
-        <option value='A+ve'>B+ve</option>
-        <option value='B+ve'>A+ve</option>
-        <option value='A-ve'>A-ve</option>
-        <option value='B-ve'>B-ve</option>
-        <option value='AB+ve'>AB+ve</option>
-        <option value='AB-ve'>AB-ve</option>
-        <option value='O+ve'>O+ve</option>
-        <option value='O-ve'>O-ve</option>
-      </select>
-      <br />
-      <button> Add Patient</button>
-    </form>
+  <>
+    <div class='bg-background'>
+      <div class='container mx-auto p-large'>
+        <form className='create p-large gradient rounded-3xl ' onSubmit={handleSubmit}>
+          <h3 class='mb-10 font-bold text-3xl'> Add a New Patient</h3>
+
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium block' htmlFor=''>First Name: </label>
+            <input 
+              class='p-2.5 text-textLight shadow rounded w-2/5  outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              type='text'
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstName}
+            />{' '}
+          </div>
+          
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium block' htmlFor=''>Last Name: </label>
+            <input
+              class='p-2.5 text-textLight shadow rounded w-2/5 outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              type='text'
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName}
+            />{' '}
+          </div>
+          
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium  block' htmlFor=''>Date of Birth: </label>
+            <input
+              class='p-2.5 text-textLight shadow rounded w-2/5 outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              type='date'
+              onChange={(e) => setDateofBirth(e.target.value)}
+              value={dateOfBirth}
+            />{' '}
+          </div>
+
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium  block' htmlFor=''>Address: </label>
+            <input
+              class='p-2.5 text-textLight shadow rounded w-2/5 outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              type='text'
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
+            />
+          </div>
+          
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium  block' htmlFor=''>Phone: </label>
+            <input 
+              class='p-2.5 text-textLight shadow rounded w-2/5 outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              type='text'
+              onChange={(e) => setPhone(e.target.value)}
+              value={phone}
+            />  
+          </div>
+                  
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium  block' htmlFor=''>Email: </label>
+            <input
+              class='p-2.5 text-textLight shadow rounded w-2/5 outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              type='email'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
+          
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium  block' htmlFor=''>Gender: </label>
+            <select
+              class='p-2.5 text-textLight shadow rounded w-2/5 outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              name=''
+              id='gender'
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <option value='Male'>Male</option>
+              <option value='Female'>Female</option>
+              <option value='Other'>Other</option>
+            </select>
+          </div>
+          
+
+          <div class='mb-3'>
+            <label class='mb-2 text-sm font-medium  block' htmlFor=''>Blood Group: </label>
+            <select
+              class='p-2.5 text-textLight shadow rounded w-2/5 outline-none focus:border-solid focus:border focus:border-primary focus:shadow-none transition'
+              name=''
+              id='gender'
+              value={gender}
+              onChange={(e) => setBloodGroup(e.target.value)}
+            >
+              <option value='A+ve'>B+ve</option>
+              <option value='B+ve'>A+ve</option>
+              <option value='A-ve'>A-ve</option>
+              <option value='B-ve'>B-ve</option>
+              <option value='AB+ve'>AB+ve</option>
+              <option value='AB-ve'>AB-ve</option>
+              <option value='O+ve'>O+ve</option>
+              <option value='O-ve'>O-ve</option>
+            </select>
+          </div>
+          
+          <button class='px-4 py-2 bg-primary hover:bg-secondary text-white rounded-full text-base mt-10 transition-colors'> Add Patient</button>
+        </form>
+      </div>
+
+    </div>
+  </>
   );
 };
 
