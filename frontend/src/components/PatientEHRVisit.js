0d7172dc65ae0c233f8f89d8bbe6bfdae84b6011
Simran Thapa <simranthapa634@gmail.com>
2023-11-09 11:06:29 -0600

    style patient details and ehr visit detail page

--- frontend/src/components/PatientEHRVisit.js
+++ frontend/src/components/PatientEHRVisit.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Dashboard from './dashboard/Dashboard';
 import { convertDate, calculateAge } from '../commons/functions';
+import avatar from '../images/avatar.png'
 
 const PatientEHRVisit = () => {
   const [ehrVisit, setEhrVisit] = useState();
@@ -55,7 +57,7 @@ const PatientEHRVisit = () => {
 
   return (
     <>
-      <div>EhrVisit Details</div>
+      {/* <div>EhrVisit Details</div>
       <div>
         Patient Name: {ehrVisit?.patient?.firstName}{' '}
         {ehrVisit?.patient?.lastName}
@@ -177,6 +179,231 @@ const PatientEHRVisit = () => {
       ) : (
         'No Admission Found'
       )}
+ */}
+
+      <div className='flex'>
+        <Dashboard />
+        <div className='bg-background w-3/4 content'>
+          <div className='container mx-auto p-large'>
+            <h3 className='mb-10 font-bold text-3xl'>
+                EhrVisit Details
+            </h3>
+            <div className='flex'>
+            <div className='p-small gradient rounded-3xl mt-10 mr-10 basis-2/4'>
+              <div className='block '>
+                <div className='card-body'>
+                  <div className='user-details-block'>
+                    <div className='user-profile flex justify-center mt-[-86px]'>
+                      <img
+                        className='h-32 w-32 overflow-hidden rounded-full border-2 border-primary object-cover '
+                        src={avatar}
+                        alt='Avatar'
+                      />
+                    </div>
+                    <div className='text-center mt-3'>
+                      <h4>
+                        <b> {ehrVisit?.patient?.firstName}{' '}
+                        {ehrVisit?.patient?.lastName}</b>
+                      </h4>
+                      <p>{calculateAge(ehrVisit?.patient?.dateOfBirth)} Years</p>
+                    </div>
+                    <h4>
+                      <b> EhrVisit Details</b>
+                    </h4>
+                    <ul className='flex items-center justify-between p-0 mt-4 mb-0 '>
+                      <li className='text-center w-1/3'>
+                        <h6 className='text-primary'>Height</h6>
+                        <h3>
+                          {ehrVisit?.height}
+                        </h3>
+                      </li>
+                      <li className='text-center border-l border-black-100 w-1/3'>
+                        <h6 className='text-primary'>Weight</h6>
+                        <h3>
+                          {ehrVisit?.weight} lbs
+                        </h3>
+                      </li>
+                      <li className='text-center border-l border-black-100 w-1/3'>
+                        <h6 className='text-primary'>Blood Pressure</h6>
+                        <h3 className='text-warning'>
+                          {ehrVisit?.bloodPressure}
+                        </h3>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div className='p-small gradient rounded-3xl mt-10 basis-2/4'>
+              <div className='block '>
+                <div className='card-body'>
+                  <div className='user-details-block'>
+                    
+                    <h4>
+                      <b> Contact Info Info</b>
+                    </h4>
+                    <ul className='flex flex-wrap items-center justify-between p-0 mt-4 mb-0 '>
+                      <li className='text-center border-l border-primary-100 w-1/2 p-1 mb-2'>
+                        <h6 className='text-primary'>Health Staff</h6>
+                        <h3>
+                          {ehrVisit?.healthStaff?.firstName} &nbsp;
+                          {ehrVisit?.healthStaff?.lastName} 
+                        </h3>
+                      </li>
+                      <li className='text-center border-l border-primary-100 w-1/2 p-1 mb-2'>
+                        <h6 className='text-primary'>Visit Date</h6>
+                        <h3>
+                          {convertDate(ehrVisit?.visitDate)}
+                        </h3>
+                      </li>
+                      <li className='text-center border-l border-primary-100 w-1/2 p-1 mb-2'>
+                        <h6 className='text-primary'>Diagnosis</h6>
+                        <h3 className='text-warning'>
+                          {ehrVisit?.diagnosis}
+                        </h3>
+                      </li>
+                      <li className='text-center border-l border-primary-100 w-1/2 p-1 mb-2'>
+                        <h6 className='text-primary'>Follow Up Instructions</h6>
+                        <h3>
+                          {ehrVisit?.followUpInstructions}
+                        </h3>
+                      </li>
+                      <li className='text-center border-l border-primary-100 w-1/2 p-1 mb-2'>
+                        <h6 className='text-primary'>Procedure</h6>
+                        <h3>
+                          {ehrVisit?.procedure}
+                        </h3>
+                      </li>
+                      <li className='text-center border-l border-primary-100 w-1/2 p-1 mb-2'>
+                        <h6 className='text-primary'>Medical History</h6>
+                        <h3>
+                          {ehrVisit?.medicalHistory}
+                        </h3>
+                      </li><li className='text-center border-l border-primary-100 w-1/2 p-1 mb-2'>
+                        <h6 className='text-primary'>Prescribed Medication</h6>
+                        <h3>
+                          {ehrVisit?.prescribedMedications}
+                        </h3>
+                      </li>
+                    </ul>
+                    {/* contact info */}
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+            {/* Lab report */}
+            <div className='p-large gradient rounded-3xl mt-10'>
+              <div className='block '>
+                <div className='card-body'>
+                  <div className='user-details-block'>
+                    <div className='text-center mb-10'>
+                      <h3>
+                        <b>Lab Results</b>
+                      </h3>
+                    </div>
+                    <ul className='p-0 mt-4 mb-0'>
+                         {ehrVisitPatientLab.length > 0
+                          ? ehrVisitPatientLab.map((lab, idx) => {
+                              return (
+                                <div key={idx}>
+                                  <p>
+                                    {lab?.lab?.name} -
+                                    <button
+                                      style={{ textDecoration: 'underline', color: 'blue' }}
+                                      onClick={(e) => {
+                                        window.open(
+                                          `http://localhost:5005/reports/${lab?.report}`,
+                                          '_blank',
+                                          'noreferrer'
+                                        );
+                                      }}
+                                    >
+                                      Show Lab Report
+                                    </button>
+                                    - {lab?.labFee} | {convertDate(lab?.date)}
+                                  </p>
+                                </div>
+                              );
+                            })
+                          : 'No Lab Test Found'}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+             {/* Medication */}
+            <div className='bg-white rounded-3xl shadow-lg p-5 text-sm mt-10'>
+              {ehrVisitMedicationList.length > 0 ? (
+                <table>
+                  <thead>
+                    <tr>
+                      <th className='p-4'> S.N.</th>
+                      <th className='p-4'>Medication Name</th>
+                      <th className='p-4'>Dosage</th>
+                      <th className='p-4'>Medication Cost</th>
+                      <th className='p-4'>Prescribed Date</th>
+                      <th className='p-4'>Health Staff</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {ehrVisitMedicationList.map((med, idx) => {
+                      return (
+                        <tr key={idx}>
+                          <td className='p-4'>{idx + 1}</td>
+                          <td className='p-4'>{med?.medicationName}</td>
+                          <td className='p-4'>{med?.dosage}</td>
+                          <td className='p-4'>${med?.medicineCost} </td>
+                          <td className='p-4'>{convertDate(med?.prescribedDate)} </td>
+                          <td className='p-4'>
+                            {med?.healthStaff.firstName} &nbsp;{' '}
+                            {med?.healthStaff.lastName}{' '}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              ) : (
+                'No Medication Found'
+              )}
+            </div>
+             {/* Admission */}
+            <div className='bg-white rounded-3xl shadow-lg p-5 text-sm mt-10'>
+              {ehrVisitAdmission.length > 0 ? (
+              <table>
+                <thead>
+                  <tr>
+                    <th className='p-4'> S.N.</th>
+                    <th className='p-4'>Bed Number</th>
+                    <th className='p-4'>Admission Date</th>
+                    <th className='p-4'>Discharge Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {ehrVisitAdmission.map((admission, idx) => {
+                    return (
+                      <tr key={idx}>
+                        <td className='p-4'>{idx + 1}</td>
+                        <td className='p-4'>
+                          {admission?.bedNumber.roomNumber.roomNumber}
+                          {admission?.bedNumber.bedNumber.bedNumber}
+                        </td>
+                        <td className='p-4'>{convertDate(admission?.admissionDate)} </td>
+                        <td className='p-4'>{convertDate(admission?.dischargeDate)} </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            ) : (
+              'No Admission Found'
+            )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/src/components/PatientHistory.js
+++ frontend/src/components/PatientHistory.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Dashboard from './dashboard/Dashboard';
 
 const PatientHistory = () => {
   const [allPatient, setAllPatient] = useState([]);
@@ -12,21 +13,33 @@ const PatientHistory = () => {
       });
     };
 
-    getAllPatient();
+    getAllPatient();  
   }, []);
   return (
     <>
-      <h1> Patient List </h1>
-      {allPatient &&
-        allPatient.map((patient, index) => {
-          return (
-            <h5 key={patient._id}>
-              <Link to={`/patient-history/${patient._id}`}>
-                {patient?.firstName + ' ' + patient?.lastName}{' '}
-              </Link>
-            </h5>
-          );
-        })}
+      <div className='flex'>
+        <Dashboard />
+        <div className='bg-background w-4/5 content'>
+          <div className='container px-5 py-medium'>
+              <h3 className='mb-10 font-bold text-3xl'>
+                 Patient List 
+              </h3>
+            <div className='bg-white rounded-3xl shadow-lg p-5 text-sm'>
+              <h4 className='p-4 mb-2 font-bold'>Patient Name</h4>
+              {allPatient &&
+                allPatient.map((patient, index) => {
+                  return (
+                    <p className='p-4' key={patient._id}>
+                      <Link className='text-secondary' to={`/patient-history/${patient._id}`}>
+                        {patient?.firstName + ' ' + patient?.lastName}{' '}
+                      </Link>
+                    </p>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
